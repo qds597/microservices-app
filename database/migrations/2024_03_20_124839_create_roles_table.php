574989a5a86bd1dd63521c903ddc1d3d756501b2
2212010012 <muhammadqaddasa@gmail.com>
2024-03-21 21:24:23 +0700

    penambahan table

--- database/migrations/2024_03_20_124839_create_roles_table.php
+++ database/migrations/2024_03_20_124839_create_roles_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('roles', function (Blueprint $table) {
             $table->id();
+            $table->string('name_roles|required|max:255|unique:roles');
             $table->timestamps();
         });
     }
