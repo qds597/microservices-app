758404a067b2b4161044afc9e04c71034d1a9daa
2212010012 <muhammadqaddasa@gmail.com>
2024-03-21 22:06:52 +0700

    upgrade

--- app/Models/Roles.php
+++ app/Models/Roles.php
@@ -11,40 +11,6 @@ class Roles extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'name_roles',
+        'nama_role',
     ];
-}
-//isikan kode berikut
-try {
-    //cek apakah request berisi nama_role atau tidak
-    $validator = Validator::make($request->all(), [
-        'nama_role' => 'required|string|max:255|unique:roles',
-    ]);
-
-    //kalau tidak akan mengembalikan error
-    if ($validator->fails()) {
-        return response()->json($validator->errors());
-    }
-
-    //kalau ya maka akan membuat roles baru
-    $data = Roles::create([
-        'nama_role' => $request->nama_role,
-    ]);
-
-    //data akan di kirimkan dalam bentuk response list
-    $response = [
-        'success' => true,
-        'data' => $data,
-        'message' => 'Data berhasil di simpan',
-    ];
-
-    //jika berhasil maka akan mengirimkan status code 200
-    return response()->json($response, 200);
-} catch (Exception $th) {
-    $response = [
-        'success' => false,
-        'message' => $th,
-    ];
-    //jika error maka akan mengirimkan status code 500
-    return response()->json($response, 500);
 }
